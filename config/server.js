fc21cdb98f5fc809b60ce792b485939b73a112a7
bsrodrigue <bsrodrigue@gmail.com>
2020-09-04 18:29:12 +0000

    First commit with DB setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '91e8f369c255c94c545b9011f964114a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dfea65fa-79a8-4c18-915d-cb3fba13bbea'
+};
