c1a1e9f15fdb3dbedb6faba1da3868044b4cd5f9
bsrodrigue <bsrodrigue@gmail.com>
2020-09-15 00:17:54 +0000

    just kill me

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url:'https://agent-strapi.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '91e8f369c255c94c545b9011f964114a'),
